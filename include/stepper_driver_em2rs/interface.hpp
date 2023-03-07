3fd0d8d2b9e318211c3e08f5ec9018fbf27febf2
Roman Kuzmenko <clairbee@guidemaze.com>
2023-03-06 20:26:35 -0800

    Refactored ROS2 devices drivers for compliance with ros2_control requirements.

--- include/stepper_driver_em2rs/interface.hpp
+++ include/stepper_driver_em2rs/interface.hpp
@@ -28,17 +28,17 @@ class Interface : public stepper_driver::Interface {
   rclcpp::Parameter param_model;
 
  protected:
-  virtual void param_ppr_get_handler_(
+  virtual bool has_velocity() override { return true; }
+  virtual void position_set_real_(double) {}
+  virtual void velocity_set_real_(double) override;
+
+  virtual rclcpp::FutureReturnCode param_ppr_get_handler_(
       const std::shared_ptr<stepper_driver::srv::ParamPprGet::Request> request,
       std::shared_ptr<stepper_driver::srv::ParamPprGet::Response> response)
       override;
-  virtual void param_ppr_set_handler_(
+  virtual rclcpp::FutureReturnCode param_ppr_set_handler_(
       const std::shared_ptr<stepper_driver::srv::ParamPprSet::Request> request,
       std::shared_ptr<stepper_driver::srv::ParamPprSet::Response> response)
-      override;
-  virtual void velocity_set_handler_(
-      const std::shared_ptr<stepper_driver::srv::VelocitySet::Request> request,
-      std::shared_ptr<stepper_driver::srv::VelocitySet::Response> response)
       override;
 
  private:
--- src/interface.cpp
+++ src/interface.cpp
@@ -21,7 +21,9 @@ Interface::Interface(rclcpp::Node *node) : stepper_driver::Interface(node) {
 
   prov_ = modbus_rtu::Factory::New(node);
 
-  RCLCPP_DEBUG(node_->get_logger(), "Interface::Interface():" " started");
+  RCLCPP_DEBUG(node_->get_logger(),
+               "Interface::Interface():"
+               " started");
 
   node->declare_parameter("stepper_driver_model", "dm556rs");
   node->get_parameter("stepper_driver_model", param_model);
@@ -46,7 +48,7 @@ Interface::Interface(rclcpp::Node *node) : stepper_driver::Interface(node) {
   RCLCPP_DEBUG(node_->get_logger(), "Interface::Interface(): ended");
 }
 
-void Interface::param_ppr_get_handler_(
+rclcpp::FutureReturnCode Interface::param_ppr_get_handler_(
     const std::shared_ptr<stepper_driver::srv::ParamPprGet::Request> request,
     std::shared_ptr<stepper_driver::srv::ParamPprGet::Response> response) {
   (void)request;
@@ -62,10 +64,13 @@ void Interface::param_ppr_get_handler_(
   response->exception_code = resp->exception_code;
   if (!resp->exception_code && resp->len) {
     response->ppr = resp->values[0];
+    return rclcpp::FutureReturnCode::SUCCESS;
   }
+
+  return rclcpp::FutureReturnCode::INTERRUPTED;
 }
 
-void Interface::param_ppr_set_handler_(
+rclcpp::FutureReturnCode Interface::param_ppr_set_handler_(
     const std::shared_ptr<stepper_driver::srv::ParamPprSet::Request> request,
     std::shared_ptr<stepper_driver::srv::ParamPprSet::Response> response)
 
@@ -80,46 +85,44 @@ void Interface::param_ppr_set_handler_(
 
   prov_->holding_register_write(req, resp);
   response->exception_code = resp->exception_code;
+
+  if (!resp->exception_code) {
+    return rclcpp::FutureReturnCode::SUCCESS;
+  }
+
+  return rclcpp::FutureReturnCode::INTERRUPTED;
 }
 
-void Interface::velocity_set_handler_(
-    const std::shared_ptr<stepper_driver::srv::VelocitySet::Request> request,
-    std::shared_ptr<stepper_driver::srv::VelocitySet::Response> response)
-
-{
+void Interface::velocity_set_real_(double velocity) {
   auto req = std::make_shared<modbus::srv::HoldingRegisterWrite::Request>();
   auto resp = std::make_shared<modbus::srv::HoldingRegisterWrite::Response>();
 
-  uint16_t velocity = ::ceil(::abs(request->velocity*60.0));
+  uint16_t velocity_val = ::ceil(::abs(velocity * 60.0));
 
-  if (velocity_last_ != velocity) {
+  if (velocity_last_ != velocity_val) {
     // req->leaf_id will be auto-filled
     req->leaf_id = 0;
     req->addr = 0x01E1;
-    req->value = velocity;
+    req->value = velocity_val;
 
     prov_->holding_register_write(req, resp);
     if (resp->exception_code) {
-      response->exception_code = resp->exception_code;
       return;
     }
   }
 
-  if (velocity == 0) {
-    response->exception_code = 0;
+  if (velocity_val == 0) {
     return;
   }
 
   req->leaf_id = 0;
   req->addr = 0x1801;
-  if (request->velocity > 0) {
+  if (velocity > 0) {
     req->value = 0x4001;
-  }
-  else {
+  } else {
     req->value = 0x4002;
   }
   prov_->holding_register_write(req, resp);
-  response->exception_code = resp->exception_code;
 }
 
 }  // namespace stepper_driver_em2rs
